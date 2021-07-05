5132b803773c3a09b2d30526140d3af44c648b0f
Hada Yoshihiro <punio@outlook.com>
2021-07-05 11:25:30 +0900

    Add sample project

---
+++ AzureTableTest/AzureDataTables/TestTableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
+
+namespace AzureTableTest.AzureDataTables
+{
+	public class TestTableEntity : ITableEntity
+	{
+		public string PartitionKey { get; set; }
+		public string RowKey { get; set; }
+		public DateTimeOffset? Timestamp { get; set; }
+		public ETag ETag { get; set; }
+		public double Value { get; set; }
+	}
+}
---
+++ AzureTableTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureTableTest
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var directory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(directory)
+				.AddJsonFile("appsettings.json", false, true);
+			var configuration = builder.Build();
+
+
+			await WindowsAzureStorage.TestTableService.SetTestData(configuration.GetConnectionString("StorageConnection"));
+
+			try
+			{
+				await AzureDataTables.TestTableService.GetTestData(configuration.GetConnectionString("StorageConnection"));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"{e.Message}");
+			}
+
+			try
+			{
+				AzureDataTables.TestTableService.GonyoGonyo();
+				await AzureDataTables.TestTableService.GetTestData(configuration.GetConnectionString("StorageConnection"));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"{e.Message}");
+			}
+		}
+	}
+}
